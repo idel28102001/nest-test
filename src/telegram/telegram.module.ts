f17b99cd2730911c44224120066c88f381407535
idel <idel28102001>
2022-04-07 11:45:53 +0300

    update

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TelegramEntity } from './entities/telegram.entity';
 import { AppUpdate } from './update/telegram.update';
 import { TelegramService } from './services/telegram.service';
-import { NewsModule } from 'src/news/news.module';
 import { NewsService } from 'src/news/services/news.service';
 import { PostsEntity } from 'src/news/entities/posts.entity';
 import { ContentEntity } from 'src/news/entities/content.entity';
@@ -11,7 +10,7 @@ import { UsersService } from 'src/users/services/users.service';
 import { UserEntity } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TelegramEntity, PostsEntity, ContentEntity, UserEntity]), NewsModule],
+  imports: [TypeOrmModule.forFeature([TelegramEntity, PostsEntity, ContentEntity, UserEntity])],
   providers: [AppUpdate, TelegramService, NewsService, UsersService],
 
 })
